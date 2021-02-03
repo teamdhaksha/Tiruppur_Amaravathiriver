3d4b9de681d93ed35f9ba3d2ae4cabbf39714f91
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-03 12:19:04 +0530

    Add files via upload

---
+++ data/FloodVulnerableZones_4.js
@@ -0,0 +1,8 @@
+var json_FloodVulnerableZones_4 = {
+"type": "FeatureCollection",
+"name": "FloodVulnerableZones_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0 }, "geometry": { "type": "Point", "coordinates": [ 77.433175001373897, 10.63975400486688 ] } }
+]
+}
---
+++ data/Towns_8.js
@@ -0,0 +1,13 @@
+var json_Towns_8 = {
+"type": "FeatureCollection",
+"name": "Towns_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Kallapuram" }, "geometry": { "type": "Point", "coordinates": [ 77.291550618799235, 10.434871079032449 ] } },
+{ "type": "Feature", "properties": { "NAME": "Karatholuvu" }, "geometry": { "type": "Point", "coordinates": [ 77.378732365100888, 10.62470258366131 ] } },
+{ "type": "Feature", "properties": { "NAME": "Dharapuram South" }, "geometry": { "type": "Point", "coordinates": [ 77.531672059146317, 10.71867170727757 ] } },
+{ "type": "Feature", "properties": { "NAME": "Dharapuram North" }, "geometry": { "type": "Point", "coordinates": [ 77.536507542069799, 10.75833573449143 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kothanur" }, "geometry": { "type": "Point", "coordinates": [ 77.58698769431291, 10.84690680187617 ] } },
+{ "type": "Feature", "properties": { "NAME": "Mayilrangam" }, "geometry": { "type": "Point", "coordinates": [ 77.757423105528659, 10.86872016578511 ] } }
+]
+}
